e9dbd53181b3b7105f7f4e324e4bc324b46dbedf
sofayam <mark.andrew@gmail.com>
2023-01-09 23:40:08 +0000

    better

--- kanjicoverage.py
+++ kanjicoverage.py
@@ -1,13 +1,28 @@
 import csv
 from collections import defaultdict
-f = open("../../GJVL.txt")
+
+def isKanji(ch):
+    res = False
+    o = ord(ch)
+    if (o >= 0x4e00) and (o <= 0x9faf):
+        res = True
+    #print (ch, ":",  res)
+    return res
+
+f = open("GJVL.txt")
 fields = ["English", "Japanese", "Reading", "Story", "Picture"]
 cf = csv.DictReader(f, fieldnames=fields, delimiter="\t")
 all = defaultdict(int)
+stories = defaultdict(int)
 for row in cf:
     words = row["Japanese"]
-    for ch in words: 
-        all[ch] +=1
+    story = row["Story"]
+    for ch in words:
+        if isKanji(ch): 
+            all[ch] +=1
+    for ch in story:
+        if isKanji(ch):
+            stories[ch] +=1
 
 nonuniq = 0
 print ("Total", len(all))
@@ -17,6 +32,18 @@ for key in all:
         # print(key)
 
 print("Non unique", nonuniq)
+
+print("With stories")
+
+all |= stories
+nonuniq = 0
+print ("Total", len(all))
+for key in all:
+    if all[key] > 1:
+        nonuniq += 1
+
+print("Non unique", nonuniq)
+
 
 
 # use ord to get number from kanji
